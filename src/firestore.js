56780829573e5bda51589a7b167cb842421fa6cc
Constantin Goeldel <constantin_goeldel@t-online.de>
2020-10-24 17:18:01 +0200

    add project_id

--- src/firestore.js
+++ src/firestore.js
@@ -2,6 +2,7 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp({
   credential: admin.credential.cert({
+    project_id: 'bike-protection',
     private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
